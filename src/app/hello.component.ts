96888cd0a607f6a4be094fdd154ed6693c3b8800
Andysimmonsisme <andysimmons78@gmail.com>
2022-06-08 09:58:46 -0400

    cosmetic fixes; pull in bootstrap css

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,15 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!`,
+  template: `<h1>Hello{{name ? ' ' + name : ''}}!`,
 })
 export class HelloComponent {
   @Input() name: string;
   getName() {
-    return prompt('Please enter your name');
+    return '';
+    //prompt('Please enter your name');
   }
   ngOnInit() {
     let nameEntered = this.getName();
-    if (nameEntered !== null && nameEntered.length > 1) this.name = nameEntered;
+    if (nameEntered !== null && nameEntered.length > 0) this.name = nameEntered;
   }
 }
